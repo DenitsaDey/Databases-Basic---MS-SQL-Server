1a984089d8334c60c1c9314353a03716e7479896
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2021-01-18 12:42:23 +0300

    Update 1. One-To-One Relationship.sql

--- Table Relations/1. One-To-One Relationship.sql
+++ Table Relations/1. One-To-One Relationship.sql
@@ -2,35 +2,28 @@ CREATE DATABASE TableRelations
 
 USE TableRelations
 
-CREATE TABLE Customers
-(
-PersonID INT PRIMARY KEY IDENTITY(1,1),
-FirstName NVARCHAR(10) NOT NULL,
-Salary DECIMAL(7,2) NOT NULL,
-PassportID INT UNIQUE NOT NULL
-)
-
-INSERT INTO Customers (FirstName, Salary, PassportID)
-VALUES
-	('Roberto', 43300.00, 102),
-	('Tom', 56100.00, 103),
-	('Yana', 60200.00, 101)
-
-
 CREATE TABLE Passports
 (
-PassportID INT PRIMARY KEY IDENTITY(101,1),
+PassportID INT PRIMARY KEY,
 PassportNumber NVARCHAR(10) NOT NULL
 )
 
-INSERT INTO Passports (PassportNumber)
+INSERT INTO Passports (PassportID, PassportNumber)
 VALUES
-	('N34FG21B'),
-	('K65LO4R7'),
-	('ZE657QP2')
+	(101, 'N34FG21B'),
+	(102, 'K65LO4R7'),
+	(103, 'ZE657QP2')
 
-ALTER TABLE Customers
-ADD CONSTRAINT FK_Customers 
-FOREIGN KEY (PassportID)
-REFERENCES Passports(PassportID)
+CREATE TABLE Persons
+(
+PersonID INT PRIMARY KEY,
+FirstName NVARCHAR(10) NOT NULL,
+Salary DECIMAL(7,2) NOT NULL,
+PassportID INT NOT NULL FOREIGN KEY REFERENCES Passports(PassportID)UNIQUE
+)
 
+INSERT INTO Persons (PersonID, FirstName, Salary, PassportID)
+VALUES
+	(1, 'Roberto', 43300.00, 102),
+	(2, 'Tom', 56100.00, 103),
+	(3, 'Yana', 60200.00, 101)
